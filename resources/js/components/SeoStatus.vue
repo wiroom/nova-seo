12a9606e0f3572a74c35629affb5ccb634923c8d
Julien Casanova <juliencasanova@gmail.com>
2024-07-24 17:23:54 +0200

    extracted SeoStatus component

---
+++ resources/js/components/SeoStatus.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { config } from '@/config'
+import { Value } from '@/types/value'
+
+const { meta, showDetails } = withDefaults(defineProps<{
+  meta: Value,
+  showDetails: boolean,
+}>(), {
+  showDetails: true,
+});
+
+const robotsEnabled = !meta.robots_noindex;
+const hasMinimumSeo = meta.title.length >= 3 && meta.title.length <= 70 && meta.description.length >= 50 && meta.description.length <= 160;
+</script>
+
+<template>
+  <div
+    :class="config.rootClass"
+    class="flex items-center gap-x-1.5"
+  >
+    <span
+      class="flex size-3 rounded-full"
+      :class="{
+          'bg-green-500': robotsEnabled && hasMinimumSeo,
+          'bg-yellow-500': robotsEnabled && !hasMinimumSeo,
+          'bg-red-500': !robotsEnabled,
+        }"
+    />
+    <span
+      v-if="showDetails && !robotsEnabled"
+      class="text-xs text-gray-500 dark:text-gray-400">No index</span>
+    <span
+      v-else-if="showDetails && !hasMinimumSeo"
+      class="text-xs text-gray-500 dark:text-gray-400">À améliorer</span>
+  </div>
+</template>
